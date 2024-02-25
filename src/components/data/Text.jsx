a672e3f09fb8f1924f442abfdf7e5607a58c5f87
Márton Szebasztián <szebasztianmarton@gmail.com>
2024-02-25 10:52:03 +0100

    Update Text.jsx

--- src/components/data/Text.jsx
+++ src/components/data/Text.jsx
@@ -8,11 +8,31 @@ export const HomeText =
         description:"As a dedicated front-end developer, I harness the power of design and creativity to craft web solutions.",
         button:"Say Hello",
         //Footer Copyright Text. 
-        // Copy new automatically
+        // The copyright year is automatically renewed
         copy:"All rights are reserved"
 
     }
 ];
+export const AboutText =
+[
+    //About.jsx TEXT
+    {
+        title:"About me",
+        subtitle:"A short introduction",
+        description:"Quickly pick up new concepts and technologies. I excel in dynamic environments and love adapting to the latest trends in frontend development.",
+        //CV Download BUTTON 
+        button:"Download CV",
+        //Box 1 
+        title1:"Experience",
+        subtitle1:"2+ Years",
+        //Box 2
+        title2:"Completed",
+        subtitle2:"8 Projects",
+        //Box 3
+        title3:"Student",
+        subtitle3:"I'm still studying at university."
+    }
+] ;
 
 //Social Links
 
@@ -29,8 +49,9 @@ export const Sociallinks =
 ];
 
 //Project Text:
-
 export const projectsData = [
+  //Badgelist: AndroidStudio, Arduino, AWS, Azure, Css, Dart, Git, HTML, Htmx, Illustrator, Java, JavaScript, JQuery, Kubernetes, Laravel, Less, MongoDB, MySQL, Netlify, 
+  //           Nginx, Node, Notion, Npm, React, Sass, Svelte, Swift, TailwindCSS, VueJS, WebAssembly, XD, Yarn 
     {
       id: 1,
       category: "React",
@@ -157,26 +178,7 @@ export const projectsData = [
 
 //About Text 
 
-export const AboutText =
-[
-    //About.jsx TEXT
-    {
-        title:"About me",
-        subtitle:"A short introduction",
-        description:"Quickly pick up new concepts and technologies. I excel in dynamic environments and love adapting to the latest trends in frontend development.",
-        //CV Download BUTTON 
-        button:"Download CV",
-        //Box 1 
-        title1:"Experience",
-        subtitle1:"2+ Years",
-        //Box 2
-        title2:"Completed",
-        subtitle2:"8 Projects",
-        //Box 3
-        title3:"Student",
-        subtitle3:"I'm still studying at university."
-    }
-]
+
 
 
 
@@ -188,16 +190,41 @@ import Project4 from "../../assets/project4.jpg";
 import Project5 from "../../assets/project5.jpg";
 import Project6 from "../../assets/project6.jpg";
 //Project Skills ICONS import 
-import React from "../../assets/skills/React-Light.svg";
+import AndroidStudio from "../../assets/skills/AndroidStudio-Light.svg";
+import Arduino from "../../assets/skills/Arduino.svg";
+import AWS from "../../assets/skills/AWS-Light.svg";
+import Azure from "../../assets/skills/Azure-Light.svg";
+import css from "../../assets/skills/CSS.svg";
+import Dart from "../../assets/skills/Dart-Light.svg";
+import Git from "../../assets/skills/Git.svg";
+import HTML from "../../assets/skills/HTML.svg";
+import Htmx from "../../assets/skills/Htmx-Light.svg";
+import Illustrator from "../../assets/skills/Illustrator.svg";
+import Java from "../../assets/skills/Java-Light.svg";
+import JavaScript from "../../assets/skills/JavaScript.svg";
+import JQuery from "../../assets/skills/JQuery.svg";
+import Kubernetes from "../../assets/skills/Kubernetes.svg";
+import Laravel from "../../assets/skills/Laravel-Light.svg";
+import Less from "../../assets/skills/Less-Light.svg";
+import MongoDB from "../../assets/skills/MongoDB.svg";
+import MySQL from "../../assets/skills/MySQL-Light.svg";
+import Netlify from "../../assets/skills/Netlify-Light.svg";
+import Nginx from "../../assets/skills/Nginx.svg";
 import Node from "../../assets/skills/NodeJS-Light.svg";
-import AndroidStudio from "../../assets/skills/AndroidStudio-Light.svg"
-import Netlify from "../../assets/skills/Netlify-Light.svg"
-import MongoDB from "../../assets/skills/MongoDB.svg"
-import Yarn from "../../assets/skills/Yarn-Light.svg"
-import Azure from "../../assets/skills/Azure-Light.svg"
-import css from "../../assets/skills/CSS.svg"
-import Dart from "../../assets/skills/Dart-Light.svg"
-import Notion from "../../assets/skills/Notion-Light.svg"
+import Notion from "../../assets/skills/Notion-Light.svg";
+import Npm from "../../assets/skills/Npm-Light.svg";
+import React from "../../assets/skills/React-Light.svg";
+import Sass from "../../assets/skills/Sass.svg";
+import Svelte from "../../assets/skills/Svelte.svg";
+import Swift from "../../assets/skills/Swift.svg";
+import TailwindCSS from "../../assets/skills/TailwindCSS-Light.svg";
+import VueJS from "../../assets/skills/VueJS-Light.svg";
+import WebAssembly from "../../assets/skills/WebAssembly.svg";
+import XD from "../../assets/skills/XD.svg";
+import Yarn from "../../assets/skills/Yarn-Light.svg";
+
+
+
 
 
 
